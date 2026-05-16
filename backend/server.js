5de93edfe12699b38a920b8ff8fb4226d5e81dfb
jesujobakayode <jesujobakayode@gmail.com>
2026-05-16 13:27:43 +0100

    ready to commit changes

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,26 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import connectDB from "./config/db.js";
-
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors({
+  origin: "*"
+}));
 
-// MIDDLEWARE
-app.use(cors());
 app.use(express.json());
 
-
-// ROUTES
 app.use("/api/products", productRoutes);
 
+connectDB();
 
-app.get("/", (req, res) => {
-  res.send("CampusBite API Running");
-});
-
-
-// SERVER
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
